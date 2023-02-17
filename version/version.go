f37ebd599a845e9d89887cabfc2049fe7c120585
Tatiana Slonimskaia <tslonims@akamai.com>
2023-02-17 14:07:32 +0100

    Add versioning

---
+++ version/version.go
@@ -0,0 +1,5 @@
+// Package version allows to handle specific provider version
+package version
+
+// ProviderVersion is set during the release process to the release version of the binary
+var ProviderVersion = "dev"
